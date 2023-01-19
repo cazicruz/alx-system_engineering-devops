8b9f9c6ec2bfe34bbc5ec964b246db539525b772
David <dcazicruz@icloud.com>
2023-01-19 12:39:08 +0000

    task0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 # The file (School) has permissions 0744, belonging to the user and group
 # www-data.
 file {'/tmp/school':
-  ensure => present,
+  ensure  => present,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
